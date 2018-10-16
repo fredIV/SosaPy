137a92598e39f4d0587b5c19e1e0bb7e9533349f
Jamie Hayes <Jdhay@10.40.15.98>
2018-10-16 16:37:02 -0400

    messed up on the decimal fields nvm

--- SosaPy/settings.py
+++ SosaPy/settings.py
@@ -54,6 +54,8 @@ ROOT_URLCONF = 'SosaPy.urls'
 
 TEMPLATES = [
     {
+
+        #attaching django backend
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')]
         ,
@@ -77,8 +79,11 @@ WSGI_APPLICATION = 'SosaPy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Sosa',
+        'USER': 'jamieh',
+        'PASSWORD': 'django',
+        'PORT': '5432'
     }
 }
 
