a276ed431b38855d3977fca2aa549cc4cd2c8d3c
Joel Witherspoon <jw09227@georgiasouthern.edu>
2018-10-25 17:52:55 -0400

    Added admin page and board creation page Bypassed login page

--- Sosa/urls.py
+++ Sosa/urls.py
@@ -3,6 +3,9 @@ from Sosa import views
 
 
 urlpatterns = [
-    path('', views.login, name='login'),
+    # Uncomment following line for deployment
+    # path('', views.login, name='login'),
+    path('', views.admin_home, name='admin_home'),
+    path('create_board/', views.create_board, name='create_board'),
     path('mobile/', views.mobile, name='mobile'),
 ]
--- Sosa/views.py
+++ Sosa/views.py
@@ -9,8 +9,23 @@ def login(request):
     # Render the login page
     return render(request, 'login.html')
 
+
 def mobile(request):
     """View function for mobile page"""
 
     # Render the mobile page
     return render(request, 'mobile.html')
+
+
+def admin_home(request):
+    """View function for admin home page"""
+
+    # Render the admin home page
+    return render(request, 'admin_home.html')
+
+
+def create_board(request):
+    """View function for create board page"""
+
+    # Render the create board page
+    return render(request, 'create_board.html')
